7721cc9172faa0cdb037945fbe1adc90ed5f128b
Jose Corral <angel.corral71@gmail.com>
2015-02-27 20:52:14 -0600

    Account class implemented - need Bank implementation - need Holder implementation

--- src/com/daBandit/Account.java
+++ src/com/daBandit/Account.java
@@ -5,10 +5,77 @@
  */
 package com.daBandit;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.io.Serializable;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyListWrapper;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.ObservableList;
+
+
+
 /**
  *
- * @author jacorral
+ * @author Jose Corral
  */
-public class Account {
+public class Account implements Serializable {
     
+   public enum Type{
+       CHECKING, SAVINGS
+   }
+    private final LongProperty balance = new SimpleLongProperty(this, "balance", 0);
+    private final ObjectProperty<Type> type = new SimpleObjectProperty<>(this, "type");
+
+    protected Account() {
+    }
+    
+    List<Double> transactions = new ArrayList<>();
+    
+    public Account (Account.Type type){
+        this.type.set(type);
+    }
+    
+    
+    private long getBalance() {
+        return balance.get();
+    }
+
+    private void setBalance(long value) {
+        balance.set(value);
+    }
+
+    private LongProperty balanceProperty() {
+        return balance;
+    }
+    
+      
+
+
+    private Type getType() {
+        return type.get();
+    }
+
+    private void setType(Type value) {
+        type.set(value);
+    }
+
+    private ObjectProperty typeProperty() {
+        return type;
+    }
+    
+   private void deposit(Double amt){
+       transactions.add(amt);
+       balance.add(amt);
+       
+   }
+   
+   public void withdrawl(Double amt){
+       transactions.add(amt);
+       balance.subtract(amt);
+   }
+   
 }
